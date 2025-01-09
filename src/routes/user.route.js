dde3901ab12d92cd78df9fe46fcb26faa7987b0d
AntimonyBullets <antimonybullets@gmail.com>
2025-01-09 23:10:26 +0530

    commit-8

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -13,7 +13,7 @@ router.route("/register").post(
             name : "coverImage",
             maxCount: 1
         }
-    ]), //adding a middleware which will act just before the execution of 'registerUser', this middlewares enables taking input of files from user. We're gonna take input 2 image files 'avatar' and 'coverImage'
+    ]), //adding a middleware which will act just before the execution of 'registerUser', this middleware enables taking input of files from user. We're gonna take input 2 image files 'avatar' and 'coverImage'
     registerUser);
 // if anyone hits api/v1/user/register with post request 'registerUser' callback is executed.
 
