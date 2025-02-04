0d0c3942cd2ee3fcf269b7226df93e6bb0eb2f47
AntimonyBullets <antimonybullets@gmail.com>
2025-02-04 16:02:57 +0530

    adding getWatchHistory and a few routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
-import {Router} from 'express';
-import {registerUser, loginUser, logoutUser, refreshAccessToken} from '../controllers/user.controller.js';
+import { Router } from 'express';
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from '../controllers/user.controller.js';
 const router = Router();
-import {upload} from '../middlewares/multer.middleware.js';
+import { upload } from '../middlewares/multer.middleware.js';
 import { verfiyJWT } from '../middlewares/auth.middleware.js';
 
 router.route("/register").post(
@@ -23,6 +23,15 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verfiyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verfiyJWT, changeCurrentPassword); //even though changing of password is an updation, 'post' method is preferred for such operations (recreating the whole document).
+router.route("/get-user").get(verfiyJWT, getCurrentUser);
+router.route("/update-account").patch(verfiyJWT, updateAccountDetails);
+router.route("/update-avatar").patch(verfiyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/update-coverImage").patch(verfiyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verfiyJWT, getUserChannelProfile);
+// the username of the channel whose profile is being looked at will be at place of ':username' in the route.
+router.route("/history").get(verfiyJWT, getWatchHistory);
 
 
 export default router;
