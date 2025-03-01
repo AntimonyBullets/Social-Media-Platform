8e3c76b25401a0c09ee9fdc425372a6498aaf4c4
AntimonyBullets <antimonybullets@gmail.com>
2025-03-01 23:03:12 +0530

    fix: reorder searchVideos route definition

--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { verfiyJWT } from '../middlewares/auth.middleware.js';
-import { deleteVideo, searchVideos, getVideoById, togglePublishStatus, updateVideo, uploadVideo } from '../controllers/video.controller.js';
+import { deleteVideo, searchVideos, getVideoById, togglePublishStatus, updateVideo, uploadVideo} from '../controllers/video.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 
 const router = Router();
@@ -23,14 +23,15 @@ router
     );
 
 router
+    .route("/search")
+    .get(searchVideos);
+    
+router
     .route("/:videoId")
     .get(getVideoById)
     .patch(upload.single('thumbnail'),updateVideo)
     .delete(deleteVideo);
 
-router
-    .route("/search")
-    .get(searchVideos);
 
 router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
 export default router;
